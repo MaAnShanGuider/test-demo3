cbcd345fcc945202c1e10a970b24856545149499
MaAnShanGuider <2388540124@qq.com>
2020-03-25 11:19:44 +0800

    update: 试试

---
+++ index.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var path = require('path');
+
+console.log(path.resolve(__dirname, './nf/'))
+
+var statInfo = fs.statSync(path.resolve(__dirname, './nf/'));
+var fileInfo = fs.readdirSync(path.resolve(__dirname, './nf/'));
+console.log(statInfo, statInfo.isDirectory(), fileInfo);
+
+var arr = [1, 2, 3];
+
+var [...arr2 ]= arr;
+
+console.log(arr2);
+
+console.log(arr.includes())
